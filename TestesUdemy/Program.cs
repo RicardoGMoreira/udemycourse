c22052037e90d86142347d6121fe595679705cd0
Ricardo Moreira <ricardomgmoreira@gmail.com>
2021-02-19 00:11:08 +0000

    Projeto iniciado, mostrando apontamentos

---
+++ TestesUdemy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestesUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string original = "abcde FGHIJ ABC abc DEFG ";
+             string s6 = original.Replace('a','x') ;
+             Console.WriteLine("Replace('a', 'x'): -" + s6 + "-"); */
+
+
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+
+
+
+        }
+    }
+}
